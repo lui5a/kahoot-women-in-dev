96d8507c889140826424131a3401399f54a445d5
Luisa Fernanda Tenorio Castilla <ltenorio@emeal.nttdata.com>
2022-01-23 17:26:28 +0100

    merged fromtraicar-branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizzComponent } from './quizz/quizz.component';
+<<<<<<< HEAD
+=======
+//import { ResultComponent } from './result/result.component';
+>>>>>>> 05bfd789201bf18898bf06609225398092267e88
 import { StartQuizComponent } from './start-quiz/start-quiz.component';
 import { UserGuard } from './user.guard';
 
@@ -8,6 +12,10 @@ const routes: Routes = [
   { path: "", redirectTo: 'welcome', pathMatch: "full" },
   { path: "welcome", component: StartQuizComponent, pathMatch: "full" },
   { path: "quizz", component: QuizzComponent, pathMatch: "full", canActivate: [UserGuard] },
+<<<<<<< HEAD
+=======
+  //{ path: "result", component: ResultComponent, pathMatch: "full", canActivate: [UserGuard] }
+>>>>>>> 05bfd789201bf18898bf06609225398092267e88
 ];
 
 @NgModule({
